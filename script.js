16a12ab671cea4b724c9956e139dfa81d6108945
timalsinab <timalsinabishal7@gmail.com>
2023-10-28 13:41:03 -0400

    I made the website for bug navigation

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Populate discussion area with content
+    var discussionText = "We found this project very helpful in understanding the way autonomous systems behave. ";
+    document.getElementById("discussion-text").textContent = discussionText;
+});
